cef56210fe6adbe6c60c7763cbf648efceae41cf
saadiinho <saadrafiqul93380@gmail.com>
2023-09-06 13:01:05 +0200

    Adjust JavaScript

--- styles.js
+++ styles.js
@@ -6,6 +6,14 @@ const goAbout = document.getElementById("goAbout");
 // Sélectionnez la div que vous souhaitez surveiller
 const about = document.getElementById("about");
 
+// Fonction pour gérer les changements de taille d'écran
+function gestionnaireDeChangementDeTaille() {
+  // Utilisez une media query pour vérifier la largeur de l'écran
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    about.classList.remove("animate__fadeOut");
+    about.classList.add("animate__fadeIn");
+  } 
+}
 // Fonction pour vérifier si la div est visible
 function estVisible(element) {
   var rect = element.getBoundingClientRect();
@@ -18,7 +26,7 @@ function estVisible(element) {
 }
 
 // Fonction pour ajouter une classe lorsque la div est visible
-function ajouterClasseQuandVisible() {
+function addClassAbout() {
   if (estVisible(about)) {
     about.classList.remove("animate__fadeOut");
     about.classList.add("animate__fadeIn");
@@ -26,10 +34,28 @@ function ajouterClasseQuandVisible() {
     about.classList.add("animate__fadeOut");
     about.classList.remove("animate__fadeIn");
   }
+  gestionnaireDeChangementDeTaille();
 }
 
+function addClassAbout() {
+    if (estVisible(about)) {
+      about.classList.remove("animate__fadeOut");
+      about.classList.add("animate__fadeIn");
+    } else {
+      about.classList.add("animate__fadeOut");
+      about.classList.remove("animate__fadeIn");
+    }
+    gestionnaireDeChangementDeTaille();
+  }
+
 // Ajoutez un gestionnaire d'événements de défilement
-window.addEventListener("scroll", ajouterClasseQuandVisible);
+window.addEventListener("scroll", addClassAbout);
+
+// Ajoutez un gestionnaire d'événements pour les changements de taille d'écran (par exemple, redimensionnement de la fenêtre)
+window.addEventListener("resize", gestionnaireDeChangementDeTaille);
+
+// Appelez la fonction une fois au chargement de la page pour gérer la classe en fonction de la taille d'écran initiale
+
 
 // Appelez la fonction une fois au chargement de la page pour vérifier l'état initial
-ajouterClasseQuandVisible();
+addClassAbout();
